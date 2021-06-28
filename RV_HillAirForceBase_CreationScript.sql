e1db27ba83e096b9765dad1e907aebfee3a889b0
Justin Newman <justin-newman@users.noreply.github.com>
2021-06-28 15:22:12 -0600

    Fixed merge issues and updated scripts to model latest ERD

--- RV_HillAirForceBase_CreationScript.sql
+++ RV_HillAirForceBase_CreationScript.sql
@@ -34,21 +34,15 @@ GO
 USE RV_HillAirForceBase
 
 CREATE TABLE CUSTOMER(
-<<<<<<< HEAD
 	CustID					int	IDENTITY(1,1)		NOT NULL,
 	CustFirstName			varchar(50)				NOT NULL,
 	CustLastName			varchar(50)				NOT NULL,
 	CustEmail				varchar(320)			NOT NULL,
 	CustPhone				varchar(15)				NOT NULL,
-=======
-	CustID						int	IDENTITY(1,1)		NOT NULL,
-	CustFirstName				varchar(50)				NOT NULL,
-	CustLastName				varchar(50)				NOT NULL,
-	CustEmail					varchar(320)			NOT NULL,
-	CustPhone					varchar(15)				NOT NULL,
->>>>>>> c1cb9e1b240c34538108522eda4d5f871583f1dd
-	ServiceStatusID				int						NOT NULL,
-	DODAffiliationID			int						NOT NULL
+	CustLastModifiedBy		varchar(10)					NULL,
+	CustLastModifiedDate	smalldatetime				NULL,
+	ServiceStatusID			int						NOT NULL,
+	DODAffiliationID		int						NOT NULL
 )
 
 CREATE TABLE CUSTOMER_PASSWORD(
@@ -60,28 +54,9 @@ CREATE TABLE CUSTOMER_PASSWORD(
 
 CREATE TABLE DOD_AFFILIATION_TYPE(
 	DODAffiliationID	int	IDENTITY(1,1)	NOT NULL,
-	DODAffilationName	varchar(50)			NOT NULL,
+	DODAffilationType	varchar(50)			NOT NULL
 )
 
-<<<<<<< HEAD
-CREATE TABLE PAYMENT(
-	PayID		int IDENTITY(1,1)	NOT NULL,
-	PayDate	smalldatetime     		NULL,
-	PayTotalCost	smallmoney		NOT NULL,
-	IsPaid			bit					NOT NULL,
-	ResID	int					NOT NULL,
-	ReasonID		int					NOT NULL,
-	PaymentTypeID	tinyint				NOT	NULL,
-	CCReference		varchar(20)				NULL,
-)
-
-CREATE TABLE PAYMENT_REASON(
-	ReasonID	int	IDENTITY(1,1)	NOT NULL,
-	ReasonName	varchar(20)			NOT NULL
-)
-
-=======
->>>>>>> c1cb9e1b240c34538108522eda4d5f871583f1dd
 CREATE TABLE LOCATION(
 	LocationID			int	IDENTITY(1,1)	NOT NULL,
 	LocationName		varchar(100)		NOT NULL,
@@ -93,27 +68,28 @@ CREATE TABLE LOCATION(
 )
 
 CREATE TABLE PAYMENT(
-	PayID			int IDENTITY(1,1)	NOT NULL,
-	PayDate			smalldatetime     		NULL,
-	IsPaid			bit					NOT NULL,
-	ReservationID	int					NOT NULL,
-	ResID			int					NOT NULL,
-	PaymentTypeID	tinyint					NULL,
-	CCReference		varchar(20)				NULL,
+	PayID				int IDENTITY(1,1)	NOT NULL,
+	PayDate				smalldatetime     	NOT NULL,
+	PayDate				smallmoney     		NOT NULL,
+	IsPaid				bit					NOT NULL,
+	ResID				int					NOT NULL,
+	PayTypeID			tinyint				NOT NULL,
+	CCReference			varchar(20)				NULL,
+	PayLastModifiedBy	varchar(10)				NULL,
+	PayLastModifiedDate	smalldatetime			NULL
 )
 
 CREATE TABLE PAYMENT_REASON(
-	ReasonID	int	IDENTITY(1,1)	NOT NULL,
-	ReasonName	varchar(20)			NOT NULL
+	PayReasonID		int	IDENTITY(1,1)	NOT NULL,
+	PayReasonName	varchar(20)			NOT NULL
 )
 
 CREATE TABLE PAYMENT_TYPE(
-	PaymentTypeID	tinyint	IDENTITY(1,1)	NOT NULL,
-	PaymentType		varchar(11)				NOT NULL
+	PayTypeID	tinyint	IDENTITY(1,1)	NOT NULL,
+	PayType		varchar(11)				NOT NULL
 )
 
 CREATE TABLE RESERVATION(
-<<<<<<< HEAD
 	ResID					int	IDENTITY(1,1)	NOT NULL,
 	ResNumAdults			tinyint				NOT NULL,
 	ResNumChildren			tinyint				NOT NULL,
@@ -126,40 +102,22 @@ CREATE TABLE RESERVATION(
 	ResVehicleLength		tinyint					NULL,
 	ResLastModifiedBy		varchar(10)				NULL,
 	ResLastModifiedDate		smalldatetime			NULL,
-
-	VehicleTypeID					tinyint				NOT	NULL,
-=======
-	ResID							int	IDENTITY(1,1)	NOT NULL,
-	ResNumAdults					tinyint				NOT NULL,
-	ResNumChildren					tinyint				NOT NULL,
-	ResNumPets						tinyint				NOT NULL,
-	ResAcknowledgeValidPets			bit						NULL,
-	ResStartDate					smalldatetime		NOT NULL,
-	ResEndDate						smalldatetime		NOT NULL,
-	ResCreatedDate					smalldatetime		NOT NULL,
-	ResComment						varchar(max)			NULL,
-	ResVehicleLength				tinyint					NULL,
-	VehicleTypeID					tinyint					null,
->>>>>>> c1cb9e1b240c34538108522eda4d5f871583f1dd
-	CustID							int					NOT NULL,
-	SiteID							tinyint				NOT NULL,
-	StatusID						int					NOT NULL
+	VehicleTypeID			tinyint				NOT	NULL,
+	CustID					int					NOT NULL,
+	SiteID					tinyint				NOT NULL,
+	ResStatusID				int					NOT NULL
 )
 
 CREATE TABLE RESERVATION_STATUS(
-	StatusID			int	identity(1,1)	NOT NULL,
-	StatusName			varchar(50)			NOT NULL,
-	StatusDescription	varchar(100)			NULL
+	ResStatusID			int	identity(1,1)	NOT NULL,
+	ResStatusName			varchar(50)			NOT NULL,
+	ResStatusDescription	varchar(100)			NULL
 )
 
 CREATE TABLE SECURITY_ANSWER(
 	AnswerID		int	identity(1,1)	NOT NULL,
 	AnswerText		varchar(50)			NOT NULL,
-<<<<<<< HEAD
-	CustID		int					NOT NULL,
-=======
 	CustID			int					NOT NULL,
->>>>>>> c1cb9e1b240c34538108522eda4d5f871583f1dd
 	QuestionID		tinyint				NOT NULL
 )
 
@@ -170,39 +128,34 @@ CREATE TABLE SECURITY_QUESTION(
 
 CREATE TABLE SERVICE_STATUS_TYPE(
 	ServiceStatusID		int	IDENTITY(1,1)	NOT NULL,
-	ServiceStatusName	varchar(20)			NOT NULL,
+	ServiceStatusType	varchar(20)			NOT NULL
 )
 
 CREATE TABLE SITE(
-	SiteID			tinyint	IDENTITY(1,1)	NOT NULL,
-	SiteNumber		varchar(5)				NOT NULL,
-	SiteLength		tinyint					NOT NULL,
-	SiteDescription	varchar(max)				NULL,
-	SiteCategoryID	tinyint					NOT NULL,
+	SiteID					tinyint	IDENTITY(1,1)	NOT NULL,
+	SiteNumber				varchar(5)				NOT NULL,
+	SiteLength				tinyint					NOT NULL,
+	SiteDescription			varchar(max)				NULL,
+	SiteCategoryID			tinyint					NOT NULL,
+	SiteLastModifiedBy		varchar(10)					NULL,
+	SiteLastModifiedDate	smalldatetime				NULL
 )
 
 CREATE TABLE SITE_CATEGORY(
-<<<<<<< HEAD
 	SiteCategoryID			tinyint	IDENTITY(1,1)	NOT NULL,
 	SiteCategoryName		varchar(50)				NOT NULL,
 	SiteCategoryDescription	varchar(max)				NULL,
 	LocationID				int						NOT NULL
-=======
-	SiteCatID			tinyint	IDENTITY(1,1)	NOT NULL,
-	SiteCatName			varchar(50)				NOT NULL,
-	SiteCatDescription	varchar(max)				NULL,
-	SiteCatCost			smallmoney				NOT NULL
->>>>>>> c1cb9e1b240c34538108522eda4d5f871583f1dd
 )
 
 CREATE TABLE SITE_RATE(
-	RateID			int	IDENTITY(1,1)	NOT NULL,
-	RateAmount		smallmoney			NOT NULL,
-	RateStartDate	smalldatetime		NOT NULL,
-	RateEndDate		smalldatetime		NOT NULL,
-	RateLastModifiedBy	varchar(50)			NULL,
-	RateLastModifiedDate	smalldatetime	NULL,
-	SiteCategoryID	tinyint				NOT NULL
+	RateID					int	IDENTITY(1,1)	NOT NULL,
+	RateAmount				smallmoney			NOT NULL,
+	RateStartDate			smalldatetime		NOT NULL,
+	RateEndDate				smalldatetime		NOT NULL,
+	RateLastModifiedBy		varchar(50)				NULL,
+	RateLastModifiedDate	smalldatetime			NULL,
+	SiteCategoryID			tinyint				NOT NULL
 )
 
 CREATE TABLE SPECIAL_EVENT(
