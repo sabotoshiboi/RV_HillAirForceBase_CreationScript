b90bfe49b3606ddea1092f8e1f1eff6c581e7858
TifLAustin <tiffanyaustin@mail.weber.edu>
2021-07-05 13:42:58 -0600

    Batch Upload Insert Files
    Added Fake Data for our table
    Changed Insert Reseed to 1 to match Batch File numbering

--- RV_HillAirForceBase_CreationScript.sql
+++ RV_HillAirForceBase_CreationScript.sql
@@ -292,7 +292,7 @@ ALTER TABLE RESERVATION
 	ADD
 	--Not sure if this one will work
 	CONSTRAINT CK_TooLongOfStay
-	CHECK ((DATEDIFF(day, ResStartDate, ResEndDate) >= 15 AND (ResStartDate BETWEEN '04-15' AND '10-15') OR ResEndDate BETWEEN '04-16' AND '10-14')),
+	CHECK ((DATEDIFF(day, ResStartDate, ResEndDate) <= 15 AND (ResStartDate BETWEEN '04-15' AND '10-15') OR ResEndDate BETWEEN '04-16' AND '10-14')),
 	CONSTRAINT CK_TooManyPets
 	CHECK (ResNumPets < 2)
 
@@ -322,115 +322,5 @@ ALTER TABLE RESERVATION
 	DEFAULT 0 FOR ResNumChildren,
 	DEFAULT 0 FOR ResNumPets,
 	DEFAULT GETDATE() FOR ResCreatedDate
-
-GO
-
---Insert Data into Lookup Tables
---------------------------------------------------------------------------------------------------------------------------
-
-DELETE FROM DOD_AFFILIATION_TYPE;
-DBCC CHECKIDENT ('DOD_AFFILIATION_TYPE', RESEED, 0)
-
-GO
--- Insert rows into table 'DOD_AFFILIATION_TYPE'
-INSERT INTO DOD_AFFILIATION_TYPE
-(
- [DODAffilationType]
-)
-VALUES ('Army'), ('Air Force'), ('Navy'), ('Marines'), ('Coast Guard')
-
-GO
-
-
-DELETE FROM SERVICE_STATUS_TYPE;
-DBCC CHECKIDENT ('SERVICE_STATUS_TYPE', RESEED, 0)
-
-GO
--- Insert rows into table 'SERVICE_STATUS_TYPE'
-INSERT INTO SERVICE_STATUS_TYPE
-(
- [ServiceStatusType]
-)
-VALUES ('Active'), ('Retired'), ('Reserves'), ('PCS')
-
-GO
-
-
-DELETE FROM PAYMENT_REASON;
-DBCC CHECKIDENT ('PAYMENT_REASON', RESEED, 0)
-
-GO
--- Insert rows into table 'PAYMENT_REASON'
-INSERT INTO PAYMENT_REASON
-(
- [PayReasonName]
-)
-VALUES ('Reservation Payment'), ('Damage'), ('Violation')
-
-GO
-
-
-DELETE FROM PAYMENT_TYPE;
-DBCC CHECKIDENT ('PAYMENT_TYPE', RESEED, 0)
-
-GO
--- Insert rows into table 'PAYMENT_TYPE'
-INSERT INTO PAYMENT_TYPE
-(
- [PayType]
-)
-VALUES ('Card'), ('Cash'), ('Check')
-
-GO
-
-
-DELETE FROM RESERVATION_STATUS;
-DBCC CHECKIDENT ('RESERVATION_STATUS', RESEED, 0)
-
-GO 
--- Insert rows into table 'RESERVATION_STATUS'
-INSERT INTO RESERVATION_STATUS
-(
- [ResStatusName]
-)
-VALUES ('Scheduled'), ('Cancelled'), ('Completed')
-
-GO
-
-INSERT INTO RESERVATION_STATUS
-(
- [ResStatusName], [ResStatusDescription]
-)
-VALUES ('Ongoing', 'Customer is expected to be on the campsite now.')
-
-GO
-
-DELETE FROM VEHICLE_TYPE;
-DBCC CHECKIDENT ('VEHICLE_TYPE', RESEED, 0)
-
-GO
-
--- Insert rows into table 'VEHICLE_TYPE'
-INSERT INTO VEHICLE_TYPE
-(
- [TypeName]
-)
-VALUES ('Motor Home'), ('Travel Trailer'), ('5th Wheel'), ('Pop Up')
-
-GO
-
-INSERT INTO VEHICLE_TYPE
-(
- [TypeName], [TypeDescription]
-)
-VALUES ('Van', 'Preferred vehicle for living down by a river')
-
-GO
-
-INSERT INTO VEHICLE_TYPE
-(
- [TypeName]
-)
-VALUES ('Other')
 
 GO
