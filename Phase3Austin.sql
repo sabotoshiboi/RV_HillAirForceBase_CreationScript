b0e4d154cb21de783bfb08f92ffc3fb97b83cce4
Austin Langston <41969793+Mulock3@users.noreply.github.com>
2021-07-26 08:22:39 -0600

    Added My Portion
    SP_charge_fee
    FN_GetCustomerPayments
    TR_Payment_PayTypeID

---
+++ Phase3Austin.sql
@@ -0,0 +1,75 @@
+USE RV_HillAirForceBase
+-- sp_charge_fee
+--  Accepts the input parameters @ReservationID and @ReasonID.  
+--  Creates a new PAYMENT record tied to a reservation.  
+GO
+IF EXISTS(SELECT * FROM INFORMATION_SCHEMA.ROUTINES WHERE SPECIFIC_NAME = 'sp_charge_fee')
+DROP PROCEDURE sp_charge_fee;
+
+GO
+
+CREATE PROC sp_charge_fee
+	@ReservationID	int,
+	@ReasonID		int,
+	@TotalCost		int,
+	@PayTypeID		int
+
+AS
+	BEGIN
+
+	INSERT INTO Payment(PayDate, PayTotalCost, IsPaid, ResID, ReasonID, PayTypeID)
+	VALUES(GETDATE(), @TotalCost, 0, @ReservationID, @ReasonID, @PayTypeID)
+
+	PRINT 'Inserted new Payment to database'
+
+	END
+
+GO
+
+-- fn_GetCustomerPayments :: Returns all receipts of customer payments and outstanding balances for a specific customer
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID('dbo.fn_GetCustomerPayments') 
+AND type in (N'FN', N'IF',N'TF', N'FS', N'FT'))
+DROP FUNCTION dbo.fn_GetCustomerPayments;
+
+GO
+
+CREATE FUNCTION dbo.fn_GetCustomerPayments(@CustomerID int)
+RETURNS TABLE
+AS
+
+RETURN
+SELECT PayID, PayDate, PayTotalCost, PayType, IsPaid FROM PAYMENT
+LEFT JOIN PAYMENT_TYPE on PAYMENT_TYPE.PayTypeID = PAYMENT.PayTypeID
+LEFT JOIN RESERVATION on RESERVATION.ResID = PAYMENT.ResID
+WHERE CustID = @CustomerID
+
+GO
+
+-- tr_Payment_PayTypeID :: On creation (or, if not required - then update) - if payment PayTypeID is a card, require a CCReference to be filled in as well.
+DROP TRIGGER IF EXISTS dbo.tr_Payment_PayTypeID
+
+GO
+
+Create Trigger tr_Payment_PayTypeID ON PAYMENT
+AFTER UPDATE, INSERT
+AS
+
+	-- If updated to card payment
+	IF (UPDATE (PaytypeID) AND NOT UPDATE (CCReference))
+	BEGIN 
+		-- Assuming we make card payments ID of 1
+		IF ((SELECT PayTypeID FROM inserted) = 1)
+		BEGIN
+			Raiserror ('Cannot update, needs CCReference for card payments', 16, 1) 
+			ROLLBACK TRAN
+		END
+	END
+
+	-- If just inserted
+	ELSE IF ((SELECT PayTypeID FROM inserted) = 1)
+	BEGIN
+		Raiserror ('Cannot insert, needs CCReference for card payments', 16, 1) 
+		ROLLBACK TRAN
+	END
+
+GO
